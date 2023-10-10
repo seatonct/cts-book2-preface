fb179d0f1aeb7a4bb1d27c348854478db9ae442e
Charles Seaton <seatonct@gmail.com>
2023-10-10 15:14:52 -0500

    finished bottoms directory

---
+++ bottoms/__init__.py
@@ -0,0 +1,4 @@
+from .dress_pants import DressPants
+from .jeans import Jeans
+from .shorts import Shorts
+from .skirt import Skirt
---
+++ bottoms/jeans.py
@@ -0,0 +1,9 @@
+"""base class for jeans"""
+
+
+class Jeans():
+    """initializes jeans instance"""
+
+    def __init__(self, wash, style):
+        self.wash = wash
+        self.style = style
